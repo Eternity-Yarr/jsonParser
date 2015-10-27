3fce1b7c0ad3d3b2f724e7e0518ca14f4066c284
Dmitry V <savraz@gmail.com>
2015-10-27 21:34:57 +0300

    Let's try to compose

---
+++ adja-tests/src/test/java/ru/nojs/json/MappingJsonParserTest.java
@@ -0,0 +1,88 @@
+package ru.nojs.json;
+
+import org.junit.Assert;
+import org.junit.Test;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.HashMap;
+
+import static org.mockito.Mockito.*;
+
+public class MappingJsonParserTest {
+    private static final MappingJsonParser mjp = mock(MappingJsonParser.class);
+    static { // replace me
+        when(mjp.parse(any(), any())).thenThrow(new NotImplementedException());
+    }
+
+    @Test
+    public void testSimpleMapper() {
+        String json = "\"abcdef\"";
+        Reader r = new StringReader(json);
+        Mapper<String> stringMapper = JSONElement::getAsString;
+        String result  = mjp.parse(r, stringMapper);
+        Assert.assertEquals("I shall not crash", "abcdef", result);
+    }
+
+    @Test
+    public void testMapMapper() {
+        String json = "{\"a\": \"abcdef\", \"b\": \"bgedf\"}";
+        Reader r = new StringReader(json);
+        Mapper<HashMap<String, String>> hashMapMapper = mock(Mapper.class, "replace me");
+        HashMap<String, String> result = mjp.parse(r, hashMapMapper);
+        Assert.assertEquals("key 'a' bounded correctly", "abcdef", result.get("a"));
+        Assert.assertEquals("key 'b' bounded correctly", "bgedf", result.get("b"));
+    }
+
+    @Test
+    public void testHeterogeneousMapper() { // Is it even possible?!
+        String json = "{\"a\": \"abcdef\", \"b\": 1234}";
+        Reader r = new StringReader(json);
+        Mapper<HashMap<String, ?>> hashMapMapper = mock(Mapper.class, "replace me");
+        HashMap<String, ?> result = mjp.parse(r, hashMapMapper);
+        Assert.assertEquals("key 'a' bounded correctly", "abcdef", result.get("a"));
+        Assert.assertEquals("key 'b' bounded correctly", 1234, result.get("b"));
+    }
+
+    @Test
+    public void testPOJOMapper() {
+        String json = "{\"name\": \"Connor McLeod\", \"age\": 4412}";
+        Human ref = new Human().setAge(4412).setName("Connor McLeod");
+        Reader r = new StringReader(json);
+        Mapper<Human> someBeanMapper = mock(Mapper.class, "replace me");
+        Human result = mjp.parse(r, someBeanMapper);
+        Assert.assertEquals("names match", "Connor McLeod", result.getName());
+        Assert.assertEquals("ages match", 4412, result.getAge());
+        Assert.assertEquals("we ve got our highlander", ref, result);
+    }
+
+    private static class Human { //TODO: incomplete.
+        private String name;
+        private long age;
+        public Human() {
+
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Human setName(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public long getAge() {
+            return age;
+        }
+
+        public Human setAge(long age) {
+            this.age = age;
+
+            return this;
+        }
+    }
+
+}
