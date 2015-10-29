844458ded46375a12b7f1d6e66a0f21a2c4bc3b9
vdovinds <d_vdovin@live.ru>
2015-10-29 20:56:39 +0300

    one more test

--- adja-tests/src/test/java/ru/nojs/json/JsonParserTest.java
+++ adja-tests/src/test/java/ru/nojs/json/JsonParserTest.java
@@ -276,7 +276,7 @@ public class JsonParserTest {
         );
 
         // Bonus level ^_^
-        // Тест
+        // Тест тест тест
         Assert.assertEquals(
                 "Strings can have these symbols, and event an escaped quotes",
                 "\t\r\n\" ", sjp.parse(new StringReader("\"\t\r\n\\\" \"")).getAsString()
