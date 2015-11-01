856565624ab3310763c282abbeedf0c32a0ef91b
ther10n73 <fear604@mail.ru>
2015-11-01 21:49:17 +0300

    4 of 4 tests in complited

---
+++ adja-json-api/src/main/java/ru/nojs/json/JSONNullImpl.java
@@ -0,0 +1,109 @@
+package ru.nojs.json;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * Created by Юыху on 01.11.2015.
+ */
+public class JSONNullImpl implements JSONNull {
+    @Override
+    public BigDecimal getAsBigDecimal() {
+        return null;
+    }
+
+    @Override
+    public BigInteger getAsBigInteger() {
+        return null;
+    }
+
+    @Override
+    public boolean getAsBoolean() {
+        return false;
+    }
+
+    @Override
+    public byte getAsByte() {
+        return 0;
+    }
+
+    @Override
+    public char getAsCharacter() {
+        return 0;
+    }
+
+    @Override
+    public double getAsDouble() {
+        return 0;
+    }
+
+    @Override
+    public float getAsFloat() {
+        return 0;
+    }
+
+    @Override
+    public int getAsInt() {
+        return 0;
+    }
+
+    @Override
+    public JSONArray getAsJsonArray() {
+        return null;
+    }
+
+    @Override
+    public JSONNull getAsJsonNull() {
+        return null;
+    }
+
+    @Override
+    public JSONObject getAsJsonObject() {
+        return null;
+    }
+
+    @Override
+    public JSONPrimitive getAsJsonPrimitive() {
+        return null;
+    }
+
+    @Override
+    public long getAsLong() {
+        return 0;
+    }
+
+    @Override
+    public Number getAsNumber() {
+        return null;
+    }
+
+    @Override
+    public short getAsShort() {
+        return 0;
+    }
+
+    @Override
+    public String getAsString() {
+        return null;
+    }
+
+    @Override
+    public boolean isJsonArray() {
+        return false;
+    }
+
+    @Override
+    public boolean isJsonNull() {
+        return false;
+    }
+
+    @Override
+    public boolean isJsonObject() {
+        return false;
+    }
+
+    @Override
+    public boolean isJsonPrimitive() {
+        return false;
+    }
+}
