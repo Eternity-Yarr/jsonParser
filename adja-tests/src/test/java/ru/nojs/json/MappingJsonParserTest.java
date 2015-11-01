a71d098ac05e734ab1e314e55087053c2891e8ef
Dmitry V <savraz@gmail.com>
2015-11-01 19:02:59 +0300

    why hashCode is important

--- adja-tests/src/test/java/ru/nojs/json/MappingJsonParserTest.java
+++ adja-tests/src/test/java/ru/nojs/json/MappingJsonParserTest.java
@@ -9,6 +9,7 @@ import ru.vdovin.jsonParser.MyJSONPrimitive;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class MappingJsonParserTest {
@@ -106,6 +107,17 @@ public class MappingJsonParserTest {
 
     }
 
+    @Test
+    public void testBrokenHashCode() {
+        Human h1 = new Human().setAge(12).setName("NoHashCode Kenny");
+        Human h2 = new Human().setAge(12).setName("NoHashCode Kenny");
+
+        Map<Human, String> someInfo = new HashMap<>();
+        someInfo.put(h1, "related data");
+        Assert.assertEquals("they all created equal", h1, h2);
+        Assert.assertEquals("Can we get data we stored?", "related data", someInfo.get(h2));
+    }
+
     private static class Human { //TODO: incomplete.
         private String name;
         private long age;
@@ -144,8 +156,5 @@ public class MappingJsonParserTest {
                     .append(name, rhs.getName())
                     .isEquals();
         }
-
-
     }
-
 }
