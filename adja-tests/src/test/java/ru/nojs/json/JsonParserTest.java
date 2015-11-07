6e49e9fe45b16167a4dee2edeb1a11d610b4455d
Dmitry V <savraz@gmail.com>
2015-11-08 01:08:59 +0300

    Few more tests

--- adja-tests/src/test/java/ru/nojs/json/JsonParserTest.java
+++ adja-tests/src/test/java/ru/nojs/json/JsonParserTest.java
@@ -4,7 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.vdovin.jsonParser.ImplementedJsonParser;
 
+import java.io.Reader;
 import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
 
 public class JsonParserTest {
@@ -32,6 +36,22 @@ public class JsonParserTest {
         // It's not a boolean, exception should be thrown
     }
 
+    @Test
+    public void testInteger() {
+        String jsonPrimitive = "-66";
+        JSONElement je = sjp.parse(new StringReader(jsonPrimitive));
+        int integer = je.getAsInt();
+        Assert.assertEquals("Negative integers parsed correctly", -66, integer);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testBadInteger() {
+        String jsonPrimitive = "6-6";
+        JSONElement je = sjp.parse(new StringReader(jsonPrimitive));
+        //Not an integer.
+    }
+
+
     @Test(expected = IllegalArgumentException.class)
     public void testSyntaxError() {
         String badSyntax = "{true";
@@ -41,8 +61,27 @@ public class JsonParserTest {
     }
 
     @Test
-    public void testJsonObjectParse() {
-        String someDict = "{\"a\": 5, \"b\": \"apples\"}";
+    public void testAnotherLittleObject() throws Exception {
+        String str = "{\"a\":true}";
+        JSONElement je = sjp.parse(new StringReader(str));
+        Assert.assertTrue("We ve got an object", je.isJsonObject());
+        JSONObject jo = je.getAsJsonObject();
+        boolean b = jo.get("a").getAsBoolean();
+        Assert.assertTrue("Boolean parsed correctly", b);
+    }
+
+    @Test
+    public void testAnotherLittleObject2() throws Exception {
+        String str = "{\"a\":null}";
+        JSONElement je = sjp.parse(new StringReader(str));
+        Assert.assertTrue("We ve got an object", je.isJsonObject());
+        JSONObject jo = je.getAsJsonObject();
+        Assert.assertTrue("Boolean parsed correctly", jo.get("a").isJsonNull());
+    }
+
+    @Test
+    public void testJsonObjectParse() throws Exception {
+        String someDict = "{\"a\":  5,\"b\": \"apples\",\"c\":\"it's work?\"}";
         JSONElement je = sjp.parse(new StringReader(someDict));
         Assert.assertTrue("We ve got object indeed", je.isJsonObject());
         JSONObject jo = je.getAsJsonObject();
@@ -50,5 +89,121 @@ public class JsonParserTest {
         Assert.assertEquals("Number parsed correctly", 5, numPrimitive.getAsInt());
         JSONPrimitive stringPrimitive = jo.get("b").getAsJsonPrimitive();
         Assert.assertEquals("String parsed correctly too", "apples", stringPrimitive.getAsString());
+        JSONPrimitive stringPrimitive1 = jo.get("c").getAsJsonPrimitive();
+        Assert.assertEquals("String parsed correctly too", "it's work?", stringPrimitive1.getAsString());
+    }
+
+    @Test
+    public void testInsignificantSymbolsInObject() throws Exception {
+        String json = "{           \"a\" : \"abcdef\",             \"b\" : \"bgedf\"}";
+        JSONElement je = sjp.parse(new StringReader(json));
+        Assert.assertTrue("We ve got object indeed", je.isJsonObject());
+        JSONObject jo = je.getAsJsonObject();
+    }
+
+    @Test
+    public void testArrayInObject() throws Exception {
+        String jsonArray = "{\"test\":[1,1,1,1]}";
+        JSONElement je = sjp.parse(new StringReader(jsonArray));
+        Assert.assertTrue("We ve got an array", je.isJsonObject());
+        JSONObject jo = je.getAsJsonObject();
+        Assert.assertTrue("We ve got an array", jo.get("test").isJsonArray());
+        JSONArray array = jo.get("test").getAsJsonArray();
+        Assert.assertEquals("It contains expected number of elements", 4, array.size());
+    }
+
+    @Test
+    public void testInsignificantSymbols() throws  Exception {
+        Assert.assertTrue(
+                "Leading spaces",
+                sjp.parse(new StringReader(" true")).getAsBoolean()
+        );
+        Assert.assertTrue(
+                "Trailing spaces",
+                sjp.parse(new StringReader("true ")).getAsBoolean()
+        );
+        Assert.assertTrue(
+                "All sorts of insignificance",
+                sjp.parse(new StringReader(" \t\t\n\n\r    true \r\t\n  ")).getAsBoolean()
+        );
+        Assert.assertTrue(
+                "Array indented weirdly",
+                sjp.parse(new StringReader("\t [\t\t\n\n\r    true , \r\t\n  false\r\t\n] \n")).getAsJsonArray().size() == 2
+        );
+
+        // Bonus level ^_^
+        Assert.assertEquals(
+                "Strings can have these symbols, and event an escaped quotes",
+                "\t\r\n\" ", sjp.parse(new StringReader("\"\t\r\n\\\" \"")).getAsString()
+        );
+    }
+
+
+    @Test // Bonus level 2 : Hard
+    public void testThreadSafety() throws Exception {
+        String jsonObject = "{\"test\":[1,1,1,1,1,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1,11,1,1]}";
+        List<Reader> rdrs = IntStream
+                .range(0, 10)
+                .mapToObj(x -> new StringReader(jsonObject))
+                .collect(Collectors.toList());
+
+        boolean res = rdrs
+                .parallelStream()
+                .map(sjp::parse)
+                .allMatch(je -> je.getAsJsonObject().get("test").getAsJsonArray().size() == 40);
+        Assert.assertTrue(res);
+    }
+
+    @Test
+    public void testEqualsPrimitive() {
+        JSONPrimitive jp1 = new JSONPrimitiveImpl("test");
+        JSONPrimitive jp2 = new JSONPrimitiveImpl("test");
+
+        Assert.assertTrue(jp1.equals(jp2));
+    }
+
+    @Test
+    public void testEqualsArray() {
+
+        JSONPrimitive jp1 = new JSONPrimitiveImpl("test");
+        JSONPrimitive jp2 = new JSONPrimitiveImpl("test");
+
+        JSONArray ja1 = new JSONArrayImpl();
+        ja1.add(jp1);
+        JSONArray ja2 = new JSONArrayImpl();
+        ja2.add(jp2);
+
+        Assert.assertTrue(ja1.equals(ja2));
+    }
+
+    @Test
+    public void testEqualsObj() {
+
+        JSONPrimitive jp1 = new JSONPrimitiveImpl("test");
+        JSONPrimitive jp2 = new JSONPrimitiveImpl("test");
+
+        JSONArray ja1 = new JSONArrayImpl();
+        ja1.add(jp1);
+        JSONArray ja2 = new JSONArrayImpl();
+        ja2.add(jp2);
+
+        JSONObject jo1 = new JSONObjectImpl();
+        jo1.add("test", ja1);
+        JSONObject jo2 = new JSONObjectImpl();
+        jo2.add("test", ja2);
+
+        Assert.assertTrue(jo1.equals(jo2));
+    }
+
+
+    @Test
+    public void testBigObj() {
+        String noSuchField =
+                "{\n" +
+                        "  \"a\": \"a\",\n" +
+                        "  \"b\": 2,\n" +
+                        "  \"c\": \"c\"\n" +
+                        "}";
+        JSONElement je = new ImplementedJsonParser().parse(new StringReader(noSuchField));
     }
 }
